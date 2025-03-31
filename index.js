e8923891df3ef5b977c61702cd3239d2397b8733
wbagazk <info.wirdiyanbagask@gmail.com>
2025-03-31 07:45:23 +0700

    ✨ Simple Website Tiktok Downloader with API

--- index.js
+++ index.js
@@ -22,12 +22,52 @@ const limiter = rateLimit({
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+    totalRequests++;
+    sendUpdateToClients();
+    next();
+});
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/monitor-page", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "monitor", "monitor.html"));
+});
+
+app.get("/monitor", (req, res) => {
+    res.set({
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+    });
+    res.flushHeaders();
+
+    const clientId = Date.now();
+    const newClient = { id: clientId, res };
+    clients.push(newClient);
+
+    res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+
+    req.on("close", () => {
+        clients = clients.filter(client => client.id !== clientId);
+    });
+});
+
+function sendUpdateToClients() {
+    clients.forEach(client => {
+        client.res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+    });
+}
 
 const routes = ["ytdl", "twitterdl", "igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "ttsearch", "ytsearch", "llama-3.3-70b-versatile", "txt2img", "ssweb", "translate", "nulis", "cekkhodam", "tahukahkamu", "brat", "qc", "detiknews", "kompasnews", "cekrekening", "cekcodebank"];
 routes.forEach(route => {
     app.use(`/api/${route}`, limiter, require(`./api/${route}`));
 });
+
+app.get("/api/endpoint-count", (req, res) => {
+    res.json({ totalEndpoints: routes.length });
+});
+
 module.exports = app;
